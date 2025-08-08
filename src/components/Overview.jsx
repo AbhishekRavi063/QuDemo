9bab751c563ab8f10b121d0691fa1df389acad5e
AbhishekRavi063 <abhishekravi063@gmail.com>
2025-08-08 15:28:14 +0530

    removed debug

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -60,6 +60,8 @@ const Overview = () => {
         </Link>
       </div>
 
+
+
       {/* Summary Cards */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         <Card
