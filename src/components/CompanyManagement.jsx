d0b9091ec53ebc310155dc0303455b368b0c9766
AbhishekRavi063 <abhishekravi063@gmail.com>
2025-09-20 13:22:00 +0530

    tset

--- src/components/CompanyManagement.jsx
+++ src/components/CompanyManagement.jsx
@@ -382,7 +382,7 @@ const CompanyManagement = () => {
                     disabled={isSubmitting}
                     className="px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 disabled:opacity-50"
                   >
-                    {isSubmitting ? 'Creating...' : 'Create Company'}
+                    {isSubmitting ? 'Creating....' : 'Create Company'}
                   </button>
                 </div>
               </form>
